6146c905d8cd2c252bc197754dd7b1710e46c416
nanaishere <87164398+nanaishere@users.noreply.github.com>
2025-02-22 15:36:37 +0300

    leson_2

---
+++ myProject/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import lesson_2.CheckSumSign;
+import lesson_2.CompareNumbers;
+import lesson_2.PrintColor;
+import lesson_2.PrintThreeWords;
+
+public class Main {
+    public static void main(String [] args) {
+        PrintThreeWords printThreeWords = new PrintThreeWords();
+        CheckSumSign checkSumSign = new CheckSumSign();
+        PrintColor printColor = new PrintColor();
+        CompareNumbers compareNumbers = new CompareNumbers();
+
+    }
+}
+
---
+++ myProject/src/main/java/lesson_2/PrintThreeWords.java
@@ -0,0 +1,9 @@
+package lesson_2;
+
+public class PrintThreeWords {
+    {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
